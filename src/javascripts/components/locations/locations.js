d50908aec1292b0bb4adbfb6e4a492521bc2ca8e
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-04-22 19:26:30 -0500

    location thangz

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -6,11 +6,42 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-warning';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
+  // domString += '<div class = "container">';
+  domString += '<div class = "d-flex row">';
   locations.forEach((location) => {
-    domString += `<h3>${location.name}</h3>`;
+    domString += `<div id = ${location.id} class="card location col-2">`;
+    domString += '<div class="card-body">';
+    domString += `<h5 class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</h5>`;
+    domString += `<img class="img-thumbnail" src=${location.imageUrl}></img>`; // thumbnail controls size
+    domString += `<p>${location.address}</p>`;
+    domString += '</div>';
+    domString += '</div>';
   });
+  domString += '</div>';
+  // domString += '</div>';
   util.printToDom('locations', domString);
 };
 
