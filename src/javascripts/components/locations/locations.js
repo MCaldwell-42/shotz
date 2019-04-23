0ebe2996bca8daedb80c279ee852979108e07fcc
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-04-22 21:09:13 -0500

    filters

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -27,11 +27,11 @@ const shootTimeClass = (shootTime) => {
   return selectedClass;
 };
 
-const domStringBuilder = () => {
+const domStringBuilder = (locArray) => {
   let domString = '';
   // domString += '<div class = "container">';
   domString += '<div class = "d-flex row">';
-  locations.forEach((location) => {
+  locArray.forEach((location) => {
     domString += `<div id = ${location.id} class="card location col-2">`;
     domString += '<div class="card-body">';
     domString += `<h5 class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</h5>`;
@@ -45,14 +45,54 @@ const domStringBuilder = () => {
   util.printToDom('locations', domString);
 };
 
+const filterButtonEvent = (e) => {
+  const buttonId = e.target.id;
+  const darkLocations = locations.filter(x => x.shootTime === 'After Dark');
+  const morningLocations = locations.filter(x => x.shootTime === 'Morning');
+  const afternoonLocations = locations.filter(x => x.shootTime === 'Afternoon');
+  const eveningLocations = locations.filter(x => x.shootTime === 'Evening');
+  switch (buttonId) {
+    case 'afterDark':
+      domStringBuilder(darkLocations);
+      break;
+    case 'morning':
+      domStringBuilder(morningLocations);
+      break;
+    case 'afternoon':
+      domStringBuilder(afternoonLocations);
+      break;
+    case 'evening':
+      domStringBuilder(eveningLocations);
+      break;
+    default:
+      domStringBuilder(locations);
+  }
+};
+
 const initializeLocations = () => {
   locationsData.getLocationsData()
     .then((resp) => {
       const locationsResults = resp.data.locations;
       locations = locationsResults;
-      domStringBuilder();
+      domStringBuilder(locations);
+      document.getElementById('afterDark').addEventListener('click', filterButtonEvent);
+      document.getElementById('morning').addEventListener('click', filterButtonEvent);
+      document.getElementById('afternoon').addEventListener('click', filterButtonEvent);
+      document.getElementById('evening').addEventListener('click', filterButtonEvent);
+      document.getElementById('all').addEventListener('click', filterButtonEvent);
     })
     .catch(err => console.error(err));
 };
 
-export default { initializeLocations };
+// function checkDark(shootTime) {
+//   return shootTime === 'After Dark';
+// }
+
+
+const afterDark = () => {
+  let Dark = [];
+  Dark = locations.filter(location => location.shootTime === 'After Dark');
+  domStringBuilder(Dark);
+};
+
+export default { initializeLocations, afterDark, filterButtonEvent };
--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -8,14 +8,13 @@ let movies = [];
 
 const domStringBuilder = () => {
   let domString = '';
-    // eslint-disable-next-line no-unused-expressions
   domString += '<div class = "container">';
   domString += '<div class = "d-flex row">';
   movies.forEach((movie) => {
     domString += `<div id = ${movie.id} class="card movie col-3">`;
     domString += '<div class="card-body">';
     domString += `<h5 class="card-title">${movie.name}</h5>`;
-    domString += `<p>${movie.genre}</p>`
+    domString += `<p>${movie.genre}</p>`;
     domString += `<p>${movie.releaseDate}</p>`;
     domString += `<p>${movie.description}</p>`;
     domString += `<p>${movie.locations.length} Locations</p>`;
